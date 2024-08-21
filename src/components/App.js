c1ae1fbedc32b8f9e6b48dd279597e3eb71ed82b
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-08-21 10:24:15 +0530

    issue fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import menuItems from '../RestaurantData';
+import React, { useState } from "react";
+import menuItems from "../RestaurantData";
 
 function App() {
   const [items, setItems] = useState(menuItems);
 
   const filterItems = (category) => {
-    if (category === 'all') {
+    if (category === "all") {
       setItems(menuItems);
     } else {
       setItems(menuItems.filter((item) => item.category === category));
@@ -13,23 +13,51 @@ function App() {
   };
 
   return (
-    <main>
+    <main id="main">
       <section className="menu section">
         <div className="title">
           <h2>Our Menu</h2>
           <div className="underline"></div>
         </div>
         <div className="btn-container">
-          <button className="filter-btn" id="filter-btn-all" onClick={() => filterItems('all')}>All</button>
-          <button className="filter-btn" id="filter-btn-1" onClick={() => filterItems('breakfast')}>Breakfast</button>
-          <button className="filter-btn" id="filter-btn-2" onClick={() => filterItems('lunch')}>Lunch</button>
-          <button className="filter-btn" id="filter-btn-3" onClick={() => filterItems('shakes')}>Shakes</button>
+          <button
+            className="filter-btn"
+            id="filter-btn-all"
+            onClick={() => filterItems("all")}
+          >
+            All
+          </button>
+          <button
+            className="filter-btn"
+            id="filter-btn-1"
+            onClick={() => filterItems("breakfast")}
+          >
+            Breakfast
+          </button>
+          <button
+            className="filter-btn"
+            id="filter-btn-2"
+            onClick={() => filterItems("lunch")}
+          >
+            Lunch
+          </button>
+          <button
+            className="filter-btn"
+            id="filter-btn-3"
+            onClick={() => filterItems("shakes")}
+          >
+            Shakes
+          </button>
         </div>
         <div className="section-center">
           {items.map((menuItem) => {
-            const { id, title, img, desc, price } = menuItem;
+            const { id, title, img, desc, price, category } = menuItem;
             return (
-              <article key={id} className="menu-item">
+              <article
+                key={id}
+                className="menu-item"
+                data-test-id={`menu-item-${category}`}
+              >
                 <img src={img} alt={title} className="photo" />
                 <div className="item-info">
                   <header>
